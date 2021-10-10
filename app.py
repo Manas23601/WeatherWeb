05d802fa4cce3769ce2da118db09c641c541b712
Manas <manas23601@gmail.com>
2021-10-10 15:00:04 +0530

    1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from .helpers import  login_required, lookup, apology
+from helpers import  login_required, lookup, apology
 # Configure application
 app = Flask(__name__)
 
